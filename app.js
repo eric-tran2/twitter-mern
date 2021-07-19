a6e41a1b17da340c1a5a477318e4595705d9b2c7
Eric Tran <eric.tran119@gmail.com>
2021-07-19 00:51:24 -0400

    fixed little errors

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-const express = require("express");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 5000;
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -9,5 +14,8 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hellooooo World :D"));
-const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
