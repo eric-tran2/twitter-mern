7f4284ae7fe7f90fe0f678e926d908f321ad76e7
Eric Tran <eric.tran119@gmail.com>
2021-07-19 01:38:32 -0400

    3/4 done with user auth

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const User = require('./models/User');
+const passport = require('passport');
+
 
 const port = process.env.PORT || 5000;
 
@@ -13,9 +16,19 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hellooooo World :D"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "eric",
+    email: "eric@eric.eric",
+    password: "ericisgreat123"
+  }) 
+  user.save()
+  res.send("Testing 1 2 1 2 Mic Check")});
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
