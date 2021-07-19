85b02c87414a566e67a480f3675ce7af817abec1
Eric Tran <eric.tran119@gmail.com>
2021-07-19 01:55:27 -0400

    validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,26 +5,30 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 // users.js
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  User.findOne({ handle: req.body.handle }).then(user => {
-    if (user) {
-      errors.handle = "User already exists";
-      return res.status(400).json(errors);
-    } else {
-      const newUser = new User({
-        handle: req.body.handle,
-        email: req.body.email,
-        password: req.body.password
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        // Use the validations to send the error
+        errors.email = 'Email already exists';
+        return res.status(400).json(errors);
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
       });
 
       bcrypt.genSalt(10, (err, salt) => {
@@ -51,39 +55,36 @@ router.post("/register", (req, res) => {
 });
 
 // users.js
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  const handle = req.body.handle;
+  const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({ handle }).then(user => {
-    if (!user) {
-      errors.handle = "This user does not exist";
-      return res.status(400).json(errors);
-    }
-
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        const payload = { id: user.id, handle: user.handle };
-
-        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
-          res.json({
-            success: true,
-            token: "Bearer " + token
-          });
-        });
-      } else {
-        errors.password = "Incorrect password";
-        return res.status(400).json(errors);
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        // Use the validations to send the error
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
-    });
-  });
-});
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' });
+          } else {
+            // And here:
+            errors.password = 'Incorrect password'
+            return res.status(400).json(errors);
+          }
+        })
+    })
+})
 
 // users.js
 // You may want to start commenting in information about your routes so that you can find the appropriate ones quickly.
